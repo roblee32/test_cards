cf261e9621fbd3073074122d2f971881f0c70a2c
robin_home <birdlee32@googlemail.com>
2025-04-16 23:39:14 +0100

    feat: move cards to be within slots

--- main.lua
+++ main.lua
@@ -16,11 +16,34 @@ function Slot:new(o)
 end
 
 
-function Card.update(self, dt)
-	local cur_x
-	local cur_y
+function Card.slot_in(self)
+	-- check each corner of the card
+	-- if any are within the dimensions of the slot
+	-- fix the position of the card to the slot
+	-- TODO a smooth animation for this
+	local corners = {
+		{x=self.x, y=self.y},
+		{x=self.x + self.width, y=self.y},
+		{x=self.x, y=self.y + self.height},
+		{x=self.x + self.width, y=self.y + self.height}
+	}
 
-	cur_x, cur_y = love.mouse.getPosition()
+	local slots = slots
+
+	for i, slot in pairs(slots) do
+		for j, corner in pairs(corners) do
+			if corner.x >= slot.x and corner.x <= slot.x + slot.width and corner.y >= slot.y and corner.y <= slot.y + slot.height then
+				self.x = slot.x
+				self.y = slot.y
+				return
+			end
+		end
+	end
+end
+
+
+function Card.update(self, dt)
+	local cur_x, cur_y = love.mouse.getPosition()
 
 	if love.mouse.isDown(1) then
 		if cur_x > self.x and cur_x < self.x + self.image:getPixelWidth() and cur_y > self.y and cur_y < self.y + self.image:getPixelHeight() then
@@ -31,6 +54,12 @@ function Card.update(self, dt)
 			self.grabbed = true
 		end
 	else
+		if self.grabbed then
+			-- if just released
+			-- snap to a slot if the card is within it
+			self:slot_in()
+		end
+
 		self.grabbed = false
 	end
 
@@ -38,8 +67,9 @@ function Card.update(self, dt)
 		self.x = cur_x + self.offset_x
 		self.y = cur_y + self.offset_y
 	end
-
 end
+
+
 
 function Card.draw(self)
 	local image_width, image_height = self.image:getDimensions()
@@ -61,8 +91,9 @@ end
 function love.load(args, unfilteredArgs)
 	love.window.setFullscreen(true)
 
+	slots = {Slot:new({x=240, y=240}), Slot:new({x=720, y=240})}
 	quizi_vos = Card:new({x=0, y=0, image = love.graphics.newImage("images/path_cards/Quizi_Vos.jpg")})
-	slots = {Slot:new({x=200, y=200}), Slot:new({x=400, y=200})}
+
 end
 
 
